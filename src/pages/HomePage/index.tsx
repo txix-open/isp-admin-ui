ec02fb7af08ac4127df420661c809b0ae6b633fe
mikheykinnv <mikheykinnv@txix.ru>
2024-04-08 12:41:42 +0800

    MSP-3134 move folders

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,17 +1,19 @@
-import {useState} from "react";
-import Modal from "src/widgets/Modal";
-import { Button, theme } from 'antd';
-import { useRole } from '@hooks/useRole.tsx';
+import { Button, theme } from 'antd'
+import { useState } from 'react'
+
+import { useRole } from '@hooks/useRole.tsx'
 
 import { PermissionKeys } from '@type/roles.type.ts'
 
-import './home.scss'
+import Modal from 'src/widgets/Modal'
+
+import './home-page.scss'
 
 const HomePages = () => {
   const { hasPermission } = useRole()
   const { useToken } = theme
   const { token } = useToken()
-    const [isOpenModal, setIsOpenModal] = useState(false)
+  const [isOpenModal, setIsOpenModal] = useState(false)
 
   const isPageAvailable = hasPermission(PermissionKeys.user_view)
 
@@ -24,9 +26,15 @@ const HomePages = () => {
       )}
       <div>Проверка шрифтов</div>
 
-      {isOpenModal && <Modal setIsOpenModal={setIsOpenModal} title="modal title"><div>modal content</div></Modal>}
+      {isOpenModal && (
+        <Modal setIsOpenModal={setIsOpenModal} title="modal title">
+          <div>modal content</div>
+        </Modal>
+      )}
 
-      <Button type="primary" onClick={() => setIsOpenModal(true)}>Open Modal</Button>
+      <Button type="primary" onClick={() => setIsOpenModal(true)}>
+        Open Modal
+      </Button>
     </main>
   )
 }
