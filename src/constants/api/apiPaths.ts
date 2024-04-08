d80f1e780ca8da5350a0f14db964da43f3e92a2c
Anastasiia <anufrievaad@txix.ru>
2024-04-08 09:40:06 +0300

    MSP-3100 add UserMenu directory with index, style and types

--- src/constants/api/apiPaths.ts
+++ src/constants/api/apiPaths.ts
@@ -1,6 +1,7 @@
-type PathKeys = 'baseUrl' | 'login'
+type PathKeys = 'baseUrl' | 'login' | 'logout'
 
 export const apiPaths: Record<PathKeys, string> = {
   baseUrl: '/',
-  login: '/api/admin/auth/login'
+  login: '/api/admin/auth/login',
+  logout: '/api/admin/auth/logout'
 }
