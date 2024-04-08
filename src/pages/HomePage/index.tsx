ab902d855eb2d83d0e5e25f92408214b56d1e1fb
mikheykinnv <mikheykinnv@txix.ru>
2024-04-08 12:19:20 +0800

    MSP-3134 connect Modal component

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,34 @@
-import './home-page.scss'
+import {useState} from "react";
+import Modal from "src/widgets/Modal";
+import { Button, theme } from 'antd';
+import { useRole } from '@hooks/useRole.tsx';
 
-export default function HomePage() {
-  return <div className="home-page">Добро пожаловать!</div>
+import { PermissionKeys } from '@type/roles.type.ts'
+
+import './home.scss'
+
+const HomePages = () => {
+  const { hasPermission } = useRole()
+  const { useToken } = theme
+  const { token } = useToken()
+    const [isOpenModal, setIsOpenModal] = useState(false)
+
+  const isPageAvailable = hasPermission(PermissionKeys.user_view)
+
+  return (
+    <main className="main" style={{ backgroundColor: token.colorBgLayout }}>
+      {isPageAvailable && (
+        <Button ghost type="primary">
+          Border
+        </Button>
+      )}
+      <div>Проверка шрифтов</div>
+
+      {isOpenModal && <Modal setIsOpenModal={setIsOpenModal} title="modal title"><div>modal content</div></Modal>}
+
+      <Button type="primary" onClick={() => setIsOpenModal(true)}>Open Modal</Button>
+    </main>
+  )
 }
+
+export default HomePages
