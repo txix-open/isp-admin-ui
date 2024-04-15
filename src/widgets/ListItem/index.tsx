f5e0251286c611a09345282361f3b7324eb6b36a
veselovnk <ves.nikita1303@gmail.com>
2024-04-15 11:50:29 +0300

    MSP-3107 create ListItem

---
+++ src/widgets/ListItem/index.tsx
@@ -0,0 +1,17 @@
+import { List, Tooltip } from 'antd'
+import { FC } from 'react'
+
+import { ListItemProps } from '@widgets/ListItem/list-item.type.ts'
+
+
+const ListItem: FC<ListItemProps> = ({ item }) => {
+  return (
+    <List.Item>
+      <Tooltip mouseEnterDelay={1} title={item.name}>
+        <List.Item.Meta title={item.name} />
+      </Tooltip>
+    </List.Item>
+  )
+}
+
+export default ListItem
