5df076b9929a0297cd18430088efee9213834926
veselovnk <ves.nikita1303@gmail.com>
2024-04-16 15:55:06 +0300

    MSP-3107 update PrivateRoute

--- src/routes/PrivateRoute/index.tsx
+++ src/routes/PrivateRoute/index.tsx
@@ -4,26 +4,16 @@ import { localStorageKeys } from '@constants/localStorageKeys.ts'
 
 import { LocalStorage } from '@utils/localStorageUtils.ts'
 
-import useRole from '@hooks/useRole.tsx'
-
 import { routePaths } from '@routes/routePaths.ts'
-
-import { PermissionKeys } from '@type/roles.type.ts'
 
 
 const PrivateRoute = () => {
-  const { hasPermission } = useRole()
-
   const location = useLocation()
   const userToken = LocalStorage.get(localStorageKeys.USER_TOKEN)
 
   if (!userToken) {
     LocalStorage.set('redirectUrl', location.pathname)
     return <Navigate to={routePaths.login} replace />
-  }
-
-  if ( location.pathname !=='/' && !hasPermission(PermissionKeys.read)) {
-    return <Navigate to={routePaths.home} replace />
   }
 
   return <Outlet />
