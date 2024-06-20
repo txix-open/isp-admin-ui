0b5a076d712c8ca0111d7e353803a45c0f0ffa73
mikheikinnv <mikheykinnv@txix.ru>
2024-06-20 15:42:00 +0300

    MSP-3487 add delete and add props to ReactJsonView

--- src/components/ConfigurationEditorJson/index.tsx
+++ src/components/ConfigurationEditorJson/index.tsx
@@ -4,26 +4,30 @@ import { ReactJsonView } from 'isp-ui-kit'
 import {Spin} from 'antd'
 import { cleanEmptyParamsObject } from '@utils/objectUtils.ts'
 
-const ConfigurationEditorJson: FC<ConfigurationEditorPropsType> = ({ bufConfig = {}, setBufConfig, isCurrentConfigLoading, JsonSchema}) => {
+const ConfigurationEditorJson: FC<ConfigurationEditorPropsType> = ({ bufConfig = {}, setBufConfig, isCurrentConfigLoading}) => {
   if (isCurrentConfigLoading) {
     return <Spin className="spin" />
   }
 
+  const handleEdit = (updated_src: any) => {
+    const newData = {
+      ...bufConfig,
+      data: { ...updated_src }
+    }
+    setBufConfig(cleanEmptyParamsObject(newData))
+  }
+
   return (
     <ReactJsonView
+      onAdd={true}
+      onDelete={({updated_src}: any) => handleEdit(updated_src)}
+
       displayDataTypes={false}
       displayObjectSize={false}
       name={null}
       enableClipboard={true}
       iconStyle="square"
-      onEdit={({updated_src}: any) => {
-            const newData = {
-              ...bufConfig,
-              data: { ...updated_src }
-            }
-            setBufConfig(cleanEmptyParamsObject(newData))
-          }
-      }
+      onEdit={({updated_src}: any) => handleEdit(updated_src)}
       src={bufConfig.data}
       sortKeys={true}
       collapsed={1}
