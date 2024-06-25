4762a0defdcb3ba940e2172ee34c3d835b4edbfb
Anastasiia <anufrievaad@txix.ru>
2024-06-25 13:15:59 +0700

    MSP-3486 update theme.ts

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,5 +1,17 @@
 import { theme } from 'antd'
 
-export const darkTheme = { algorithm: theme.darkAlgorithm }
+export const darkTheme = {
+  algorithm: theme.darkAlgorithm,
+  cssVar: true,
+  token: {
+    colorBgLayout: '#000000'
+  }
+}
 
-export const lightTheme = { algorithm: theme.defaultAlgorithm }
+export const lightTheme = {
+  algorithm: theme.defaultAlgorithm,
+  cssVar: true,
+  token: {
+    colorBgLayout: '#FFFFFF'
+  }
+}
